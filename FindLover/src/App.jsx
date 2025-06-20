a7e7ba79e3fb38cbf5ea1762e43f8b6f6cf7ac34
BaboJunYoung <junyoung090115@gmail.com>
2025-06-20 10:11:24 +0900

    451 error

---
+++ FindLover/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+        <div id="mainContainer">
+            <div id="text">안녕하세요. 저희는 사용자님들의 애인을 찾기위해 서비스를 하고있습니다. 아래 버튼을 눌러 애인을 찾아보세요!<br/>
+            Hello everyone! We made this web page for you to find your lover. Press the button to find lover!<br/>
+            您好，我们正在为用户提供找对象的服务。 按下面的按钮找对象吧！<br/>
+            您好，我們正在爲用戶提供找對象的服務。 按下面的按鈕找對象吧！<br/>
+            Hola. Estamos haciendo un servicio para encontrar a su novia. ¡Pulse el botón de abajo para encontrar a su novia!<br/>
+            こんにちは。私たちはユーザー様の恋人を探すためにサービスをしています。 下のボタンを押して恋人を見つけてください！<br/>
+            Bonjour. Nous fournissons un service pour trouver votre petite amie. Appuyez sur le bouton ci-dessous pour trouver votre petite amie.<br/>
+            Hallo, wir bieten unseren Service an, um Ihre Liebsten zu finden. Drücken Sie die Taste unten, um Ihren Liebhaber zu finden!<br/>
+            !مرحبًا. في هذه الخدمة التي نقدمها، لنعثر على عاشق المستخدمين لدينا. اضغط على الزر أدناه للعثور على حبيبك<br/>
+            สวัสดีค่ะ พวกเรากำลังบริการหาแฟนของคุณผู้ใช้บริการอยู่ค่ะ กรุณากดปุ่มด้านล่างเพื่อตามหาคนรักค่ะ!</div>
+            <button id="findLoverButton">애인 찾기</button>
+        </div>
+    </>
+  )
+}
+
+export default App
